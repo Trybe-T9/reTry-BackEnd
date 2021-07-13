9895d7d620c81938d5997ef66badb3d88a1370fa
lcgoandete <lcgoandete@hotmail.com>
2021-07-13 16:27:43 -0300

    feat - add endpoints in questions routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
+
 
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
