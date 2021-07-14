c454edaa44c5278c01c69e4769f00b4111dec2de
lcgoandete <lcgoandete@hotmail.com>
2021-07-14 18:54:52 -0300

    feat - add cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,7 +16,7 @@ app.use(routes);
 
 app.use((err, _req, res, _next) => {
   if (err.status) return res.status(err.status).json(err.message);
-  res.status(500).json({ message: `Erro interno: ${err.message}` });
+  res.status(500).json({ message: `Internal Error: ${err.message}` });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
