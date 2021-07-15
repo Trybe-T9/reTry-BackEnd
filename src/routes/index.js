65f683a8b873ee1ed110262397fbf094735a3715
lcgoandete <lcgoandete@hotmail.com>
2021-07-14 21:37:51 -0300

    feat - add ping route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const routes = require('express').Router();
 const usersRoute = require('./users');
 const questionsRoute = require('./questions');
 
+routes.use('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
+
 routes.use('/questions', questionsRoute);
 routes.use('/users', usersRoute);
 
--- src/routes/questions.js
+++ src/routes/questions.js
@@ -7,14 +7,15 @@ routes.get('/fundamentos', rescue(questions.getFundamentos));
 routes.get('/frontend', rescue(questions.getFrontEnd));
 
 routes.get('/all', rescue(questions.getFrontEnd));
-routes.get('/query', rescue(questions.getFrontEnd));
+routes.get('/search', rescue(questions.getFrontEnd));
 routes.get('/random', rescue(questions.getFrontEnd));
-routes.get('/module', rescue(questions.getFrontEnd));
-routes.get('/module/:id', rescue(questions.getFrontEnd));
-routes.get('/module/difficulty', rescue(questions.getFrontEnd));
-routes.get('/module/theme', rescue(questions.getFrontEnd));
-routes.get('/module/block', rescue(questions.getFrontEnd));
-routes.get('/module/type', rescue(questions.getFrontEnd));
-routes.get('/module/random', rescue(questions.getFrontEnd));
+
+// routes.get('/module', rescue(questions.getFrontEnd));
+// routes.get('/module/:id', rescue(questions.getFrontEnd));
+// routes.get('/module/difficulty', rescue(questions.getFrontEnd));
+// routes.get('/module/theme', rescue(questions.getFrontEnd));
+// routes.get('/module/block', rescue(questions.getFrontEnd));
+// routes.get('/module/type', rescue(questions.getFrontEnd));
+// routes.get('/module/random', rescue(questions.getFrontEnd));
 
 module.exports = routes;
