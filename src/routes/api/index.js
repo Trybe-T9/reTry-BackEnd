3f2f6eac5a7769a40e0d03edb9dd63b4dfdfd9fe
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-07-19 09:50:05 -0400

    Adiciona eslint

---
+++ src/routes/api/index.js
@@ -0,0 +1,17 @@
+const routes = require('express').Router();
+
+const usersRoute = require('./usersRoute');
+const questionsRoute = require('./questionsRoute');
+const loginRoute = require('./loginRoute');
+
+routes.use('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
+routes.use('/questions', questionsRoute);
+routes.use('/users', usersRoute);
+routes.use('/login', loginRoute);
+
+routes.use((err, _req, res, _next) => {
+  if (err.status) return res.status(err.status).json(err.message);
+  res.status(500).json({ message: `Internal Error: ${err.message}` });
+});
+
+module.exports = routes;
--- src/routes/api/questionsRoute.js
+++ src/routes/api/questionsRoute.js
@@ -1,7 +1,7 @@
 const rescue = require('express-rescue');
 const routes = require('express').Router();
 
-const questions = require('../controllers/questionsController');
+const questions = require('../../controllers/questionsController');
 
 routes.get('/fundamentos', rescue(questions.getFundamentos));
 routes.get('/frontend', rescue(questions.getFrontEnd));
