28853a75dc2c436415b3dacb4cbbe31fe8541f98
lcgoandete <lcgoandete@hotmail.com>
2021-07-19 22:14:00 -0300

    fix pm2 ecosystemconfig2

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -10,7 +10,7 @@ routes.use('/users', usersRoute);
 routes.use('/login', loginRoute);
 
 routes.use((err, _req, res, _next) => {
-  if (err.status) return res.status(err.status).json(err.message);
+  if (err.status) return res.status(err.status).json({ message: err.message });
   res.status(500).json({ message: `Internal Error: ${err.message}` });
 });
 
